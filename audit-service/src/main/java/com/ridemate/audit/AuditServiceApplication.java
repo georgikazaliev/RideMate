0acd8db44b8df07a7e759e2180d317ab26b46ec9
georgikazaliev <georgikazaliev@gmail.com>
2025-12-04 21:38:06 +0200

    feat: added logger for AuditServiceApplication

--- audit-service/src/main/java/com/ridemate/audit/AuditServiceApplication.java
+++ audit-service/src/main/java/com/ridemate/audit/AuditServiceApplication.java
@@ -1,5 +1,7 @@
 package com.ridemate.audit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,8 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class AuditServiceApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuditServiceApplication.class);
+
     public static void main(String[] args) {
+        logger.info("[Application] Starting RideMate Audit Service...");
         SpringApplication.run(AuditServiceApplication.class, args);
+        logger.info("[Application] RideMate Audit Service started successfully");
     }
 
 }
